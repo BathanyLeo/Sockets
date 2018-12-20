30603912bbb4fde5f841a7a5763238a033db9b3b
BathanyLeo <32330338+BathanyLeo@users.noreply.github.com>
2018-12-20 11:52:19 +0100

    Add files via upload

---
+++ Sockets Controle.py
@@ -0,0 +1,14 @@
+import socket
+
+UDP_IP = "10.160.108.101"
+UDP_PORT = 5005
+
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+sock.connect((UDP_IP,UDP_PORT))
+
+sock.send("cinema".encode())
+
+msg=sock.recv(1024)
+print(msg.decode())
+	
